19e6ad9173a1759e10f3bbe5f88526cc8c1e09f8
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-11-15 19:33:19 +0200

    add routes

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
+  const verifikation_id = localStorage.getItem("verifikation_id");
+  return verifikation_id ? Component : <Navigate to={redirectTo} />;
+};
+PrivateRoute.propTypes = {
+  component: PropTypes.any.isRequired,
+  redirectTo: PropTypes.string.isRequired,
+};
