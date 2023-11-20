6e0d84911549bd1d8c006ab392dedb93c1ea150a
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-11-20 21:06:03 +0200

    fix filter and active list

--- src/components/Archive/ArchiveTable.jsx
+++ src/components/Archive/ArchiveTable.jsx
@@ -110,7 +110,16 @@ export const ArchiveTable = () => {
           }
           return false;
         });
-
+        unique.forEach(it=> {
+          if(!it.application_number){it.application_number = ''};
+          if(!it.patientINN) {it.patientINN = ''}
+          if(!it.patientFullName) {it.patientFullName = ''};
+          if(!it.employeeID) {it.employeeID = ''};
+          if(!it.checkStatus) {it.checkStatus = ''};
+          if(!it.numberHospital) {it.numberHospital = ''};
+          if(!it.startTimeAutoHh) {it.startTimeAutoHh = ''};
+          if(!it.startTimeAutoHh) {it.startTimeAutoMm = ''}
+      })
         setUniqueChecklists(uniqueIdentifiers);
         setChecklists(unique);
         setFilterChecklists(unique);
@@ -175,11 +184,12 @@ export const ArchiveTable = () => {
         item.patientFullName
           ?.split('')
           .join('')
+          .toLowerCase()
           .includes(filters['filterPatientFIO']) &&
-        // item.numberHospital // при появлении параметров раскоммитить
-        //   ?.toString()
-        //   .toLowerCase()
-        //   .includes(filters['filterHospital']) &&
+        item.numberHospital // при появлении параметров раскоммитить
+          ?.toString()
+          .toLowerCase()
+          .includes(filters['filterHospital']) &&
         item.employeeID
           ?.split('')
           .join('')
@@ -188,10 +198,8 @@ export const ArchiveTable = () => {
           ?.toString()
           .toLowerCase()
           .includes(filters['filterStatusChecklist']) &&
-        new Date(Number(item.identifier))
-          ?.toISOString()
-          .split(':')
-          .join('')
+          moment(new Date(+item.identifier)).format(
+            'DD.MM.YYYY')
           .includes(filters['filterDateStartChecklist']) &&
         time?.join('').includes(filters['filterTimeStartChecklist'])
         //&&
@@ -532,7 +540,7 @@ export const ArchiveTable = () => {
                     </Link>
                   </TableData>
                   {item.application_number ? (
-                    <TableData>№ {item.application_number}</TableData>
+                    <TableData>{item.application_number}</TableData>
                   ) : (
                     <TableData></TableData>
                   )}
